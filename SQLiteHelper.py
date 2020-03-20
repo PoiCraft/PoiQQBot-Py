003d132f6542aecb4b4bf2da5fb7b15d62c06811
gggxbbb <2331490629@qq.com>
2020-03-21 00:43:38 +0800

    Create SQLiteHelper

---
+++ SQLiteHelper.py
@@ -0,0 +1,74 @@
+import sqlite3
+
+import config
+
+db = sqlite3.Connection(config.DATABASE)
+cursor = db.cursor()
+
+class Error(Exception):
+    pass
+class MissingParameterException(Error):
+    "参数缺失"
+    def __init__(self,msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+class TooManyPlayersException(Error):
+    "出现相同账号"
+    def __init__(self,msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+class PlayerNotFoundException(Error):
+    "玩家不存在"
+    def __init__(self,msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+
+class Player:
+    "A helper to mamager the palyers in database"
+
+    def __add(self):
+        cursor.execute('INSERT INTO GameToQQData (QQNumber, GamerName) VALUES (\'%s\', \'%s\')' %(self.__qq, self.__id))
+        cursor.commit()
+
+    def __get(self,key,value):
+        cursor.execute('select * from GameToQQData where %s=\'%s\''%(key,value))
+        self.__result = cursor.fetchall()
+
+    def __init__(self,QQNumber:str=None,GamerName:str=None):
+        if (QQNumber == None and GamerName == None):
+            raise MissingParameterException("请提供 QQ 号或 Xbox ID 中的至少一个参数")
+        self.__qq = QQNumber
+        self.__id = GamerName
+        if (self.__qq is not None):
+            self.__get("QQNumber",self.__qq)
+        else:
+            self.__get("GamerName",self.__id)
+        if (self.__qq and self.__id):
+            print('jecjejjf')
+            if len(self.__result) == 0:
+                self.__add()
+            else:
+                raise TooManyPlayersException('此QQ已被绑定过了')
+        elif len(self.__result) == 1:
+            self.__qq = self.__result[0][0]
+            self.__id = self.__result[0][1]
+        else:
+            raise PlayerNotFoundException('无法找到此玩家')
+
+    def QQNumber(self):
+        return self.__qq
+
+    def GamerName(self):
+        return self.__id
+
+    def __str__(self):
+        return self.__id
+
+    def __int__(self):
+        return int(self.__qq)
+
+
+
