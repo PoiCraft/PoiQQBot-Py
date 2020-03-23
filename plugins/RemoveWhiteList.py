f34539e7a73748781c40b45588b0737a4b99a3d9
xinyunaha <xinyunlk@outlook.com>
2020-03-23 21:20:18 +0800

    bug fixed

--- plugins/RemoveWhiteList.py
+++ plugins/RemoveWhiteList.py
@@ -10,7 +10,7 @@ __plugin_usage__ = r"""删白名单(仅管理及群主可用)
 或者 #rmw 游戏ID"""
 
 
-@on_command('rmw', aliases='删白名单', only_to_me=False)
+@on_command('rmw', aliases='删白名单', only_to_me=False, permission=permission.GROUP_OWNER | permission.GROUP_ADMIN)
 async def Bind(session: CommandSession):
     SenderQQNumber = session.ctx['user_id']  # 取发送者的qq号
     SenderGamerName = session.current_arg_text.strip()  # 去空格取命令参数
@@ -22,7 +22,7 @@ async def Bind(session: CommandSession):
         except:
             await session.send('[CQ:at,qq={0}] 服务器去火星了,等会儿再试试吧'.format(SenderQQNumber))
         ws.send(('whitelist remove %s' % SenderGamerName))
-        time.sleep(0.6)
+        time.sleep(0.1)
         result = ws.recv()
         if result == "Player removed from whitelist":
             await session.send('[CQ:at,qq=%s] %s已经从Poicraft的白名单中消失了呢!' % (SenderQQNumber, SenderGamerName))
