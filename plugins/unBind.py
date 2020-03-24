c9c8c2d1ef7978c91c05a42f6ebba866258ec60e
xinyunaha <xinyunlk@outlook.com>
2020-03-24 11:40:17 +0800

    bug fixed

--- plugins/unBind.py
+++ plugins/unBind.py
@@ -1,4 +1,6 @@
 import re
+import time
+
 from helper.SQLiteHelper import Player
 from nonebot import on_command, CommandSession,permission
 
@@ -8,17 +10,17 @@ __plugin_usage__ = r"""解除绑定(仅管理及群主可用)
 或者 #unbind 艾特一个人"""
 
 
-@on_command('unbind', aliases='解绑', only_to_me=False, permission=permission.GROUP_OWNER | permission.GROUP_ADMIN)
+@on_command('unbind', aliases='解绑', only_to_me=False)
 async def Bind(session: CommandSession):
-    SenderQQNumber = session.ctx['user_id']  # 取发送者的qq号
-    SenderMessage = session.current_arg_text.strip()
-    if not SenderMessage:
-        await session.send('[CQ:at,qq={0}]#unbind后面必须跟上@的用户嗷，例：/unbind @helloworld'.format(SenderQQNumber))
-    else:
-        AtQQNumber = re.findall(r"(?<=\[CQ:at,qq=).*?(?=\])", SenderMessage)[0]
-        ATGamerNumber = Player(QQNumber=AtQQNumber).GamerName()
-        await session.send(f'[CQ:at,qq={SenderQQNumber}] 已解除{AtQQNumber}与{ATGamerNumber}的绑定!')
+    SenderQQNumber = session.ctx['user_id']
+    SenderAtQQNumber = session.ctx['message']
+    try:
+        AtQQNumber = re.findall(r"(?<=\[CQ:at,qq=).*?(?=\])", str(SenderAtQQNumber))[0]
         try:
-            Player(QQNumber=AtQQNumber).remove()
+            SqlGamerName = Player(QQNumber=AtQQNumber).GamerName()
+            Player(AtQQNumber).remove()
+            await session.send(f'[CQ:at,qq={SenderQQNumber}] 成功解除{AtQQNumber}与{SqlGamerName}的绑定!!!')
         except Player.PlayerNotFoundException:
-            await session.send(f'[CQ:at,qq={SenderQQNumber}] 解绑失败，你都没有绑定呢!')
+            await session.send(f'[CQ:at,qq={SenderQQNumber}] 该用户没有绑定呢!!!')
+    except:
+        await session.send(f'[CQ:at,qq={SenderQQNumber}] #unbind后面艾特一个用户... 例如 #bind @user')
