801a3c7b0717528672a70346f497bbdd32ff8bd7
xinyunaha <xinyunlk@outlook.com>
2020-03-24 22:24:20 +0800

    add ban user

---
+++ plugins/SleepBan.py
@@ -0,0 +1,16 @@
+import nonebot
+from nonebot import on_command, CommandSession
+
+__plugin_name__ = '晚安套餐'
+__plugin_usage__ = r"""晚安套餐
+例：#晚安
+或者 #sleep"""
+
+bot = nonebot.get_bot()
+
+
+@on_command('sleep', aliases='晚安', only_to_me=False)
+async def Bind(session: CommandSession):
+    SenderQQNumber = session.ctx['user_id']
+    await bot.set_group_ban(group_id='769885907', user_id=f'{SenderQQNumber}', duration=6 * 60 * 60)
+    await session.send(f'[CQ:at,qq={SenderQQNumber}] 晚安，做个好梦，明天也要元气满满嗷!')
