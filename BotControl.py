5100d6e55dde0ef00f2fda8bba595aa2cf96fcc8
topjohncian <topjohncian@gmail.com>
2020-03-29 11:49:19 +0800

    use orm.

--- BotControl.py
+++ BotControl.py
@@ -7,9 +7,6 @@ from sqlalchemy.orm import sessionmaker
 import config
 
 if __name__ == '__main__':
-    engine = create_engine(
-        'mysql+pymysql://%s:%s@$%s/%s' % (config.DB_USER, config.DB_PASS, config.DB_HOST, config.DB_NAME), echo=True)
-    session = sessionmaker(bind=engine)()
     nonebot.init(config)
     nonebot.load_plugins(
         path.join(path.dirname(__file__), 'plugins'),
--- plugins/Query.py
+++ plugins/Query.py
@@ -1,24 +1,27 @@
+from nonebot import on_command, CommandSession, permission
+from sqlalchemy.orm.exc import NoResultFound
+
 import config
-from helper.DatabaseHelper import Player
-from nonebot import on_command, CommandSession,permission
 
 __plugin_name__ = '查询信息'
 __plugin_usage__ = r"""查询信息
 例：#查询
 或者 #info"""
 
+from database.Player import Player
+
 
 @on_command('info', aliases='查询', only_to_me=False, permission=permission.GROUP)
 async def Bind(session: CommandSession):
     SenderQQNumber = session.ctx['user_id']
     SenderGroupNumber = session.ctx['group_id']
+    DbSession = config.SESSION()
     if str(SenderGroupNumber) in config.SendGroup:
         pass
     else:
         try:
-            SqlGamerName = Player(QQNumber=SenderQQNumber).GamerName()
-            player = Player(SenderQQNumber)
-            await session.send(f'[CQ:at,qq={SenderQQNumber}] 您绑定了{SqlGamerName}\n传送次数{player.TpCount()}/{config.RandomTp}!!!')
-            player.TpCount()
-        except:
+            player = DbSession.query(Player).filter(Player.QQNumber == SenderQQNumber).one()
+            await session.send(
+                f'[CQ:at,qq={SenderQQNumber}] 您绑定了{player.GamerName}\n传送次数{player.TpNumber}/{config.RandomTp}!!!')
+        except NoResultFound:
             await session.send(f'[CQ:at,qq={SenderQQNumber}] 您没有绑定呢!!!')
